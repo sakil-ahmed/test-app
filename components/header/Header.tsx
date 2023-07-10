1c92c51dd98686d60c959df209b4cde89935343c
sakil-ahmed <sakilahmed1495@gmail.com>
2023-07-10 20:22:44 +0600

    update logo

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { MdClose } from "react-icons/md";
 import Menubar from "./navbar/Navbar";
-import logo from "./../../assets/logo.png";
+import logo from "./../../assets/name-logo.png";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -13,18 +13,19 @@ const Header = () => {
     <header className="header" id="header">
       <div className="container__fluid">
         <div className="row">
+          
+          <div className="logo">
+            <a href={"#home"}>
+              <Image src={logo} alt="Sakil Logo" priority={true} />
+            </a>
+          </div>
           <div className="mobile__menu__bar">
-            <button
+          <button
               onClick={() => setIsMenuOpen(true)}
               className="mobile__menu__bar__btn"
             >
               <HiOutlineMenuAlt1 />
             </button>
-          </div>
-          <div className="logo">
-            <a href={"#home"}>
-              <Image src={logo} alt="Sakil Logo" priority={true} />
-            </a>
           </div>
           <nav className={`nav__menu ${isMenuOpen && "nav__menu__open"}`}>
             <div className="close__menu">
